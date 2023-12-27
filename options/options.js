28dd0514a82f8ed36c87dd068d1b210a244e2c46
Omar Al <omaral121510@gmail.com>
2023-12-26 19:47:20 -0500

    completed project

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "holland_insurance_g_29700Identifier",
+  url: "https://holland_insurance_g_29700Identifier.botics.co",
+  api: "https://holland_insurance_g_29700Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
